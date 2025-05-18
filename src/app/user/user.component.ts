281764943f5ebfcea6435ecc1401a8229cf891d0
Hossam-Sayed <hossamsayed3030@gmail.com>
2025-05-18 11:17:04 +0300

    Add events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,12 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -10,12 +18,31 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
   // Add input decorator
+  // Receive the ID as input
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  // Event
+  // Store the value of the event in 'select'
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectedUser() {}
+  // Pass the ID back to the parent component when it is clicked
+  onSelectedUser() {
+    this.select.emit(this.id);
+  }
+
+  // Event flow is as follows:
+
+  // 1. You have access to the user's ID via the above Input id field
+
+  // 2. You bind the user click with emitting a new event in the
+  // UserComponent via the above onSelectedUser function
+
+  // 3. You bind the select event emitting with the onUserSelect
+  // funciton defined in the AppComponent wich receives the user's ID and logs it
 }
